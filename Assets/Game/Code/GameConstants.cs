45bccae1b6cef37629c9711330dd10be494c865a
xanril <motionsmith.ph@gmail.com>
2015-09-18 09:55:12 +0800

    Static classes to hold player session and game constants

---
+++ Assets/Game/Code/GameConstants.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameConstants {
+
+	public const int MAX_TILE_WIDTH = 100;
+	public const int MAX_TILE_HEIGHT = 100;
+	public const int TILE_SPACING = 20;
+}
---
+++ Assets/Game/Code/Models/PlayerSession.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerSession {
+
+	private static PlayerSession instance = null;
+
+	public static PlayerSession GetInstance() {
+
+		if(instance == null) {
+			instance = new PlayerSession();
+		}
+
+		return instance;
+	}
+
+	// Local Implementations ----------------------------------------------------
+
+	public int selectedLevel = 0;
+	public int farthestLevel = 0;
+
+
+	private PlayerSession() {
+
+		selectedLevel = 0;
+		farthestLevel = 0;
+	}
+}
