196cd0d6032cfd930f5747746245ee31c70d67b6
xanril <motionsmith.ph@gmail.com>
2015-09-23 09:33:06 +0800

    Updated ingame scripts for instantiating game elements from online data.

---
+++ Assets/Game/Scripts/Ingame/ColorStreamController.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+using DG.Tweening;
+
+public class ColorStreamController : MonoBehaviour {
+
+	private ColorStream colorStreamData;
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void SetColorStreamData(ColorStream data) {
+
+		this.colorStreamData = data;
+	}
+
+	public void StartMoving() {
+	
+		float startPosX = 0;
+		float startPosY = 0;
+		bool isHorizontal = this.colorStreamData.IsDirectionHorizontal();
+
+		if(isHorizontal) {
+
+			Vector3 vecEnd = Vector3.zero;
+
+			if(this.colorStreamData.direction == MOVE_DIRECTION.MOVE_RIGHT) {
+			
+				startPosX = GameConstants.EDGE_LEFT;
+				startPosY = IngameUtils.GetInstance().GetStreamStartPositionFromLine(this.colorStreamData.startingLine, isHorizontal);
+
+				vecEnd = new Vector3(GameConstants.EDGE_RIGHT, startPosY, 0);
+			}
+			else if(this.colorStreamData.direction == MOVE_DIRECTION.MOVE_LEFT) {
+
+				startPosX = GameConstants.EDGE_RIGHT;
+				startPosY = IngameUtils.GetInstance().GetStreamStartPositionFromLine(this.colorStreamData.startingLine, isHorizontal);
+
+				vecEnd = new Vector3(GameConstants.EDGE_LEFT, startPosY, 0);
+			}
+
+			this.gameObject.transform.localPosition = new Vector3(startPosX, startPosY, 0);
+			Tweener tmpTweener = this.gameObject.transform.DOLocalMove(vecEnd, IngameUtils.GetInstance().speedHoriz);
+			tmpTweener.SetLoops(-1);
+			tmpTweener.SetEase(Ease.Linear);
+		}
+	}
+}
--- Assets/Game/Scripts/Ingame/CoreGameController.cs
+++ Assets/Game/Scripts/Ingame/CoreGameController.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class CoreGameController : MonoBehaviour {
 
@@ -7,10 +8,25 @@ public class CoreGameController : MonoBehaviour {
 	public GameObject parentGameTiles;
 
 	[SerializeField]
+	public GameObject parentColorStreams;
+
+	[SerializeField]
 	public GameObject tileGameObject;
 
+	[SerializeField]
+	public GameObject captureTileGameObject;
+
+	[SerializeField]
+	public GameObject goalTileObject;
+
+	[SerializeField]
+	public GameObject colorStreamObject;
+
+	private GameLevel gameLevel = null;
 	private float startPosX = 0;
 	private float startPosY = 0;
+
+	public List<ColorStreamController> listColorStreamControllers;
 
 	// Use this for initialization
 	void Start () {
@@ -19,7 +35,19 @@ public class CoreGameController : MonoBehaviour {
 
 	void OnLevelWasLoaded() {
 
+		int selectedLevel = PlayerSession.GetInstance().selectedLevel;
+		this.gameLevel = GameLevelData.GetInstance().GetGameLevel(selectedLevel);
+		
+		int maxRow = this.gameLevel.maxRow;
+		int maxCol = this.gameLevel.maxCol;
+		Debug.Log("Row: " + maxRow + " Col:" + maxCol);
+		
+		IngameUtils.GetInstance().CalculateStartPositions(maxRow, maxCol);
+		this.startPosX = IngameUtils.GetInstance().tileStartPosX;
+		this.startPosY = IngameUtils.GetInstance().tileStartPosY;
+
 		InitializeLevelTiles();
+		InitializeColorStreams();
 	}
 	
 	// Initialization -------------------------------------------
@@ -28,34 +56,116 @@ public class CoreGameController : MonoBehaviour {
 	// Creates Tile prefabs by reading the GameLevel Data
 	private void InitializeLevelTiles() {
 
-		int selectedLevel = PlayerSession.GetInstance().selectedLevel;
-		GameLevel gameLevel = GameLevelData.GetInstance().GetGameLevel(selectedLevel);
-
-		int maxRow = gameLevel.maxRow;
-		int maxCol = gameLevel.maxCol;
-		Debug.Log("Row: " + maxRow + " Col:" + maxCol);
-
-		this.CalculateStartPositions(maxRow, maxCol);
-
 		int maxTileCount = gameLevel.listTile.Count;
 		for(int i = 0; i < maxTileCount; i++) {
 
 			LevelTile tmpLevelTile = gameLevel.listTile[i];
-			GameObject tmpTile = Instantiate(tileGameObject, Vector3.zero, Quaternion.identity) as GameObject;
+
+			// determine if tile type
+			GameObject tmpTile = null;
+			switch(tmpLevelTile.tileType) {
+
+			case TILE_TYPE.CAPTURE:
+				tmpTile = InstantiateCaptureTile(tmpLevelTile); break;
+
+			case TILE_TYPE.GOAL:
+				tmpTile = InstantiateGoalTile(tmpLevelTile); break;
+
+			case TILE_TYPE.BLOCK:
+				tmpTile = InstantiateBlockTile(tmpLevelTile); break;
+			
+			case TILE_TYPE.BLANK:
+				Debug.Assert(false, "Blank Tile Types should not exist when instantiating tile prefabs. Check level data and try again.");
+				break;
+			}
+
 			tmpTile.transform.parent = parentGameTiles.transform;
-			tmpTile.transform.localPosition = new Vector3(this.startPosX + tmpLevelTile.gridX * 150, 
-			                                              this.startPosY + tmpLevelTile.gridY * 150);
+			tmpTile.transform.localPosition = new Vector3(this.startPosX + (tmpLevelTile.gridX * (GameConstants.MAX_TILE_WIDTH + GameConstants.TILE_SPACING)), 
+			                                              this.startPosY - (tmpLevelTile.gridY * (GameConstants.MAX_TILE_HEIGHT + GameConstants.TILE_SPACING)));
 		}
 	}
 
-	private void CalculateStartPositions(int maxRow, int maxCol) {
-		 
-		this.startPosX = maxCol * (GameConstants.MAX_TILE_WIDTH + GameConstants.TILE_SPACING);
-		this.startPosX = this.startPosX - GameConstants.TILE_SPACING + (GameConstants.MAX_TILE_WIDTH * 0.5f);
-		this.startPosX = 0 - (this.startPosX * 0.5f);
+	private GameObject InstantiateBlockTile(LevelTile LevelTile) {
 
-		this.startPosY = maxRow * (GameConstants.MAX_TILE_HEIGHT + GameConstants.TILE_SPACING);
-		this.startPosY = this.startPosY - (GameConstants.MAX_TILE_HEIGHT * 0.5f) + GameConstants.TILE_SPACING;
-		this.startPosY = 0 - (this.startPosY * 0.5f);
+		GameObject tmpTile = Instantiate(tileGameObject, Vector3.zero, Quaternion.identity) as GameObject;
+		return tmpTile;
+	}
+
+	private GameObject InstantiateCaptureTile(LevelTile levelTile) {
+
+		GameObject tmpTile = Instantiate(captureTileGameObject, Vector3.zero, Quaternion.identity) as GameObject;
+		return tmpTile;
+	}
+
+	private GameObject InstantiateGoalTile(LevelTile levelTile) {
+
+		GameObject tmpTile = Instantiate(captureTileGameObject, Vector3.zero, Quaternion.identity) as GameObject;
+		SpriteRenderer sprRenderer = tmpTile.GetComponent<SpriteRenderer>();
+		sprRenderer.color = GameConstants.GetColor(levelTile.tileColor);
+
+		return tmpTile;
+	}
+
+	private void InitializeColorStreams() {
+
+		listColorStreamControllers = new List<ColorStreamController>();
+
+		int maxStreamCount = gameLevel.listColorStream.Count;
+		for(int i = 0; i < maxStreamCount; i++) {
+
+			ColorStream colorStream = gameLevel.listColorStream[i];
+			GameObject colorStreamObj = Instantiate(colorStreamObject, Vector3.zero, Quaternion.identity) as GameObject;
+			colorStreamObj.transform.parent = parentColorStreams.transform;
+
+			ColorStreamController clrStreamController = colorStreamObj.GetComponent<ColorStreamController>();
+			clrStreamController.SetColorStreamData(colorStream);
+			clrStreamController.StartMoving();
+			listColorStreamControllers.Add(clrStreamController);
+		}
+
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
