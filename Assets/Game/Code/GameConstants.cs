83ed558410d0d2794c1d3507bdfc442067901921
xanril <motionsmith.ph@gmail.com>
2015-09-23 09:31:38 +0800

    Added Singleton implementations of reusable functions and constant data.

--- Assets/Game/Code/GameConstants.cs
+++ Assets/Game/Code/GameConstants.cs
@@ -5,5 +5,30 @@ public class GameConstants {
 
 	public const int MAX_TILE_WIDTH = 100;
 	public const int MAX_TILE_HEIGHT = 100;
-	public const int TILE_SPACING = 20;
+	public const int TILE_SPACING = 50;
+
+	public const float EDGE_LEFT = -1000;
+	public const float EDGE_RIGHT = 1000;
+	public const float EDGE_TOP = 700;
+	public const float EDGE_DOWN = -700;
+
+	public static Color GetColor(GAME_COLORS gameColor) {
+
+		Color outputColor = Color.cyan;
+		Color.TryParseHexString("#000000", out outputColor);
+
+		switch(gameColor) {
+
+		case GAME_COLORS.BLACK:		Color.TryParseHexString("#000000", out outputColor); break;
+		case GAME_COLORS.BLUE:		Color.TryParseHexString("#0099ff", out outputColor); break;
+		case GAME_COLORS.GREEN:		Color.TryParseHexString("#33cc00", out outputColor); break;
+		case GAME_COLORS.ORANGE:	Color.TryParseHexString("#ff9900", out outputColor); break;
+		case GAME_COLORS.RED:		Color.TryParseHexString("#ff3300", out outputColor); break;
+		case GAME_COLORS.VIOLET:	Color.TryParseHexString("#9900ff", out outputColor); break;
+		case GAME_COLORS.WHITE:		Color.TryParseHexString("#ffffff", out outputColor); break;
+		case GAME_COLORS.YELLOW:	Color.TryParseHexString("#ffff00", out outputColor); break;
+		}
+
+		return outputColor;
+	}
 }
---
+++ Assets/Game/Code/IngameUtils.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IngameUtils {
+
+	private static IngameUtils instance = null;
+
+	public static IngameUtils GetInstance() {
+
+		if(instance == null) {
+			instance = new IngameUtils();
+		}
+
+		return instance;
+	}
+
+
+	// Local Implementations ---------------------------------------------------------------------------------------
+
+	public float tileStartPosX = 0;
+	public float tileStartPosY = 0;
+
+	public float speedHoriz = 10; 		// seconds
+
+	private IngameUtils() {
+
+		// Constructor
+	}
+
+	public void CalculateStartPositions(int maxRow, int maxCol) {
+		
+		this.tileStartPosX = (maxCol - 1) * (GameConstants.MAX_TILE_WIDTH) * 0.5f * -1;
+		this.tileStartPosX = this.tileStartPosX - (GameConstants.TILE_SPACING * (maxCol - 1) * 0.5f);
+		
+		/*this.tileStartPosY = maxRow * (GameConstants.MAX_TILE_HEIGHT + GameConstants.TILE_SPACING);
+		this.tileStartPosY = this.tileStartPosY - (GameConstants.MAX_TILE_HEIGHT * (maxRow % 2) * 0.5f);
+		this.tileStartPosY = this.tileStartPosY - (GameConstants.TILE_SPACING * (maxRow - 1) * 0.5f);
+		this.tileStartPosY = (this.tileStartPosY * 0.5f);*/
+
+		this.tileStartPosY = (maxRow - 1) * (GameConstants.MAX_TILE_HEIGHT * 0.5f);
+		this.tileStartPosY = this.tileStartPosY + (GameConstants.TILE_SPACING * (maxRow - 1) * 0.5f);
+	}
+
+	public float GetStreamStartPositionFromLine(int lineNumber, bool isHorizontal) {
+
+		lineNumber = lineNumber - 1; // not zero indexed
+		float startPosition = 0;
+
+		if(isHorizontal) {
+
+			startPosition = tileStartPosY - ((GameConstants.MAX_TILE_HEIGHT + GameConstants.TILE_SPACING) * 0.5f);
+			//startPosition = startPosition + ((GameConstants.MAX_TILE_HEIGHT + GameConstants.TILE_SPACING) * lineNumber);
+		}
+		else {
+
+			//startPosition = tileStartPosX - ((GameConstants.MAX_TILE_WIDTH + GameConstants.TILE_SPACING) * 0.5f);
+			//startPosition = startPosition + ((GameConstants.MAX_TILE_WIDTH + GameConstants.TILE_SPACING) * lineNumber);
+		}
+
+		return startPosition;
+	}
+}
